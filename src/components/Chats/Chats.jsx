cc51355c5af29c7c163e2677752f27945b3718de
gabtongol <geybtongol@gmail.com>
2024-11-06 20:02:39 -0500

    create chat

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -3,6 +3,7 @@ import { useState, useContext, useEffect } from "react";
 import { AuthedUserContext } from "../../App";
 
 import * as userProfileService from "/src/services/userProfileService";
+import * as chatService from "/src/services/chatService";
 import { Link } from 'react-router-dom';
 
 
@@ -14,31 +15,37 @@ const Chats = () => {
 
     useEffect(() => {
         const fetchMutualFollowing = async () => {
-          
             const { user: {followers, following }} = await userProfileService.userProfile(user._id);
-    
             const mutualFollowings = following.filter(({ id }) =>
               followers.some(follower => follower.id === id)
             );
-
             setMutuals(mutualFollowings);
             console.log('moots', mutualFollowings)
-
-        }
-        
+        } 
         if (user) fetchMutualFollowing();
     }, [user])
     
+
+    const handleCreateChat = async (mutualId) => {
+      const chat = await chatService.createChat(user._id, mutualId);
+      console.log('newchat', chat);
+    }
+
+
   return (
     <>
     
+    {/* if user clicks on a mutual, it will create new chat */}
     <p>Mutuals:</p>
     {mutuals.map((mutual, idx) => (
       <div key={idx}>
-        <p> {mutual.firstname} {mutual.lastname}</p>
+         <Link to={`/chats/${user._id}/${mutual._id}`} onClick={() => handleCreateChat(mutual._id)}>
+            <p> {mutual.firstname} {mutual.lastname}</p>
+         </Link>
       </div>
     ))}
 
+  
     
     </>
   )
--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,2 +1,21 @@
-const BASE_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/message`;
+const BASE_URL_MESSAGE = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/message`;
+const BASE_URL_CHAT = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/chat`;
 
+const createChat = async(user1, user2) => {
+    try {
+        const res = await fetch(`${BASE_URL_CHAT}/${user1}/${user2}`, {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json',
+          },
+        })
+        return await res.json();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    createChat,
+}
